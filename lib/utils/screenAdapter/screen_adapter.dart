fd7eb06d1c1a5894bb4794e53507baa8d160a897
jahn <jahnli@163.com>
2019-09-01 01:06:26 +0800

    添加注释

--- lib/utils/screenAdapter/screen_adapter.dart
+++ lib/utils/screenAdapter/screen_adapter.dart
@@ -1,10 +1,10 @@
 import 'package:flutter_screenutil/flutter_screenutil.dart';
-
+// 屏幕适配
 class ScreenAdapter {
 
   // 初始化
   static init (context){
-    //假如设计稿是按iPhone6的尺寸设计的(iPhone6 750*1334) 
+    //假如设计稿是按iPhone6的尺寸设计的(iPhone6 750*1334)
     ScreenUtil.instance = ScreenUtil(width: 750, height: 1334)..init(context);
   }
 
@@ -17,14 +17,14 @@ class ScreenAdapter {
   static height (double val){
     return ScreenUtil.getInstance().setHeight(val);
   }
-  
+
    // 获取设备高度 - DP
   static getScreenHeight(){
-    return ScreenUtil.screenHeightDp; 
+    return ScreenUtil.screenHeightDp;
   }
   // 获取设备宽度 - DP
   static getScreenWidth(){
-    return ScreenUtil.screenWidthDp; 
-  } 
+    return ScreenUtil.screenWidthDp;
+  }
 
 }
