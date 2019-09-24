395378675acaf5fe1b7fb67a09428b837fe86788
Jahn <jahnli@163.com>
2019-09-24 14:43:51 +0800

    制作口碑榜详情页

--- lib/pages/tabs/book_movie/movie/movieTop/movie_top_detail.dart
+++ lib/pages/tabs/book_movie/movie/movieTop/movie_top_detail.dart
@@ -1,8 +1,11 @@
 import 'dart:io';
-
 import 'package:dio/dio.dart';
 import 'package:flutter/material.dart';
+import 'package:flutter_jahn_douban/routes/application.dart';
 import 'package:flutter_jahn_douban/weiget/base_loading.dart';
+import 'package:flutter_jahn_douban/utils/screenAdapter/screen_adapter.dart';
+import 'package:flutter_rating_bar/flutter_rating_bar.dart';
+
 class MovieTopDetail extends StatefulWidget {
   @override
   _MovieTopDetailState createState() => _MovieTopDetailState();
@@ -13,6 +16,36 @@ class _MovieTopDetailState extends State<MovieTopDetail> {
   // 一周口碑电影榜
   Map _weekPraise;
   String _requestStatus = '';
+
+  // 控制滚动
+  ScrollController _scrollController = ScrollController();
+  // 是否展开
+  bool _isExpand = true;
+  int _total;
+  // 主题
+  bool _isDark;
+  Color _baseTextColor;
+
+  @override
+  void initState() { 
+    super.initState();
+    _baseTextColor = _isDark == true ? Colors.white:Colors.black;
+    _getWeekPraiseList();
+    // 监听滚动 控制标题栏样式
+    _scrollController.addListener((){
+      setState(() {
+        _isExpand = _scrollController.offset > 140 ? false:true;
+      });
+    });
+  }
+
+  @override
+  void dispose() { 
+    super.dispose();
+    _scrollController.dispose();
+  }
+
+  
 
   // 获取一周口碑电影榜
   _getWeekPraiseList()async{
@@ -27,6 +60,7 @@ class _MovieTopDetailState extends State<MovieTopDetail> {
         setState(() {
           _weekPraise = res.data; 
           _requestStatus = '获取豆瓣榜单详情页成功';
+          _total = res.data['total'];
         });
       }
     } catch (e) {
@@ -39,28 +73,205 @@ class _MovieTopDetailState extends State<MovieTopDetail> {
 
   }
 
-  @override
-  void initState() { 
-    super.initState();
-    _getWeekPraiseList();
-  }
+
 
   @override
   Widget build(BuildContext context) {
     return Scaffold(
       body: _requestStatus.isNotEmpty ?  NestedScrollView(
-        headerSliverBuilder: (context,val){
+        controller: _scrollController,
+        headerSliverBuilder: (context,isScroll){
           return [
             SliverAppBar(
+              backgroundColor: Colors.white,
+              iconTheme: IconThemeData(
+                color:_isExpand ? Colors.white: Colors.black
+              ),
+              brightness: Brightness.dark,
               pinned: true,
+              expandedHeight: 200,
               flexibleSpace: FlexibleSpaceBar(
-                title: Text('${_weekPraise['subject_collection']['name']}'),
+                title: AnimatedDefaultTextStyle(
+                  duration: Duration(seconds:1),
+                  style: TextStyle(color: _isExpand ? Colors.white:Colors.black,fontSize: 18),
+                  child: Text('${_weekPraise['subject_collection']['name']}'),
+                ),
+                background: Container(
+                  child: Stack(
+                    children: <Widget>[
+                      Opacity(
+                        opacity: 0.7,
+                        child: Image.network('${_weekPraise['subject_collection']['header_bg_image']}',fit: BoxFit.cover,height: 200 +  MediaQuery.of(context).padding.top),
+                      ),
+                      Positioned(
+                        top: 100,
+                        left: 30,
+                        child: Text('${_weekPraise['subject_collection']['description']}',style: TextStyle(color: Colors.white)),
+                      )
+                    ],
+                  ),
+                ),
               ),
             )
           ];
         },
-        body: Text('x'),
+        body: Container(
+          child: ListView(
+            padding: EdgeInsets.all(0),
+            children: <Widget>[
+              Container(
+                margin: EdgeInsets.only(left:ScreenAdapter.width(30),right:ScreenAdapter.width(30),top: ScreenAdapter.height(30)),
+                child: Row(
+                  children: <Widget>[
+                    Text('片单列表 · 共$_total部',style: TextStyle(color: Colors.grey)),
+                    Row(
+                      children: <Widget>[
+                      ],
+                    )
+                  ]
+                ),
+              ),
+              ListView.builder(
+                padding: EdgeInsets.all(0),
+                shrinkWrap: true,
+                physics: NeverScrollableScrollPhysics(),
+                itemBuilder: (ctx,index){
+                  return Column(
+                    children: <Widget>[
+                      Container(
+                        margin: EdgeInsets.only(left:ScreenAdapter.width(30),right:ScreenAdapter.width(30),top: ScreenAdapter.height(30)),
+                        alignment: Alignment.centerLeft,
+                        child: Container(
+                          padding: EdgeInsets.only(left: 10,right: 10,top: 3,bottom: 3),
+                          decoration: BoxDecoration(
+                            borderRadius: BorderRadius.circular(4),
+                            color: index == 0 ? Color.fromRGBO(225, 102, 119, 1):index == 1 ? Colors.orange :index == 2 ?  Color.fromRGBO(255, 193, 93, 1):Color.fromRGBO(209, 206, 201, 1)
+                          ),
+                          child: Text('No.${index+1}',style: TextStyle(color: Colors.white)),
+                        ),
+                      ),
+                      Container(
+                        margin: EdgeInsets.only(left:ScreenAdapter.width(30),right:ScreenAdapter.width(30),top: ScreenAdapter.height(30)),
+                        padding: EdgeInsets.only(bottom: ScreenAdapter.height(20)),
+                        child:GestureDetector(
+                          onTap: (){
+                            Application.router.navigateTo(context, '/movieDetail?id=${_weekPraise['subject_collection_items'][index]['id']}');
+                          },
+                          child: Row(
+                            crossAxisAlignment: CrossAxisAlignment.start,
+                            children: <Widget>[
+                              // 缩略图
+                              _thumb(_weekPraise['subject_collection_items'][index]), 
+                              // 中间信息区域
+                              SizedBox(width: ScreenAdapter.width(30)),
+                              _info(_weekPraise['subject_collection_items'][index]),
+                              SizedBox(width: ScreenAdapter.width(30)),
+                              // 右侧操作区域
+                              _actions(_weekPraise['subject_collection_items'][index])
+                            ],
+                          ),
+                        )
+                      ),
+                      Container(
+                        decoration: BoxDecoration(
+                          borderRadius: BorderRadius.circular(4),
+                          color: Color.fromRGBO(243, 243, 243, 1),
+                        ),
+                        height: ScreenAdapter.height(60),
+                        padding: EdgeInsets.only(left: ScreenAdapter.width(15)),
+                        margin: EdgeInsets.only(left:ScreenAdapter.width(30),right:ScreenAdapter.width(30),bottom: ScreenAdapter.height(30)),
+                        alignment: Alignment.centerLeft,
+                        child: Text('${_weekPraise['subject_collection_items'][index]['rating']['count']}评价',style: TextStyle(color: Color.fromRGBO(0, 0, 0, 0.5),fontSize: 12)),
+                      ),
+                      Container(
+                        height: ScreenAdapter.height(20),
+                        color: Color.fromRGBO(235, 235, 235, 1),
+                      )
+                    ],
+                  );
+                },  
+                itemCount: _weekPraise['subject_collection_items'].length,
+              )
+            ],
+          ),
+        )
       ):BaseLoading(type: _requestStatus),
     );
   }
+ 
+   // 左侧缩略图
+  Widget _thumb(item){
+    return ClipRRect(
+      borderRadius: BorderRadius.circular(8),
+      child: Image.network('${item['cover']['url']}',width: ScreenAdapter.width(200),height: ScreenAdapter.height(240),fit: BoxFit.cover,),
+    );
+  }
+  // 中间信息区域
+  Widget _info(item){
+    return Expanded(
+      child: Container(
+        constraints: BoxConstraints(
+          minHeight: ScreenAdapter.height(240)
+        ),
+        child: Column(
+          children: <Widget>[
+            Container(
+              alignment: Alignment.centerLeft,
+              margin: EdgeInsets.only(bottom: ScreenAdapter.height(10)),
+              child: Text('${item['title']}',maxLines: 1,overflow: TextOverflow.ellipsis,style: TextStyle(fontSize: 20,fontWeight: FontWeight.w400)),
+            ),
+            Row(
+              children: <Widget>[
+                RatingBarIndicator(
+                  rating:item['rating']['value'] / 2,
+                  alpha:0,
+                  unratedColor:Colors.grey,
+                  itemPadding: EdgeInsets.all(0),
+                  itemBuilder: (context, index) => Icon(
+                      Icons.star,
+                      color: Colors.amber,
+                  ),
+                  itemCount: 5,
+                  itemSize: 11,
+                ),
+                SizedBox(width: ScreenAdapter.width(15)),
+                Text('${item['rating']['value']}',style: TextStyle(color: Colors.grey,fontSize: 12))
+              ],
+            ),
+            SizedBox(height: ScreenAdapter.width(15)),
+            Text('${item['card_subtitle']}',style: TextStyle(color: Colors.grey,fontSize: 12))
+          ],
+        ),
+      ), 
+    );
+  }
+  // 右侧操作区域
+  Widget _actions(item){
+    return Container(
+      height: ScreenAdapter.height(180),
+      child: Column(
+        mainAxisAlignment: MainAxisAlignment.center,
+        children: <Widget>[
+          GestureDetector(
+            onTap: (){
+
+            },
+            child: Column(
+              children: <Widget>[
+                GestureDetector(
+                  onTap: (){
+
+                  },
+                  child: Icon(Icons.favorite_border,size: 18,color: Colors.orange),
+                ),
+                SizedBox(height: ScreenAdapter.height(10)),
+                Text('想看',style: TextStyle(fontSize: 12,color: Colors.orange))
+              ],
+            ),
+          ),
+        ],
+      )
+    );
+  }
+
 }
