b13e5e68ffcd7316dfc0b6ea83b32193a98b4a07
Jahn <jahnli@163.com>
2019-10-11 15:10:48 +0800

    添加上拉操作菜单

--- lib/pages/tabs/home/home.dart
+++ lib/pages/tabs/home/home.dart
@@ -1,14 +1,72 @@
 import 'package:flutter/material.dart';
+import 'package:solid_bottom_sheet/solid_bottom_sheet.dart';
+
 class HomePage extends StatefulWidget {
   @override
   _HomePageState createState() => _HomePageState();
 }
 
 class _HomePageState extends State<HomePage> {
+
+  SolidController _controller = SolidController();
+
   @override
   Widget build(BuildContext context) {
-    return Container(
-      child: Text('首页'),
+    return Scaffold(
+      appBar: AppBar(
+        title: Text("Solid bottom sheet example"),
+      ),
+      body: ListView.builder(
+        itemCount: 20,
+        itemBuilder: (context, index) {
+          return Card(
+            child: Column(
+              mainAxisSize: MainAxisSize.min,
+              children: <Widget>[
+                Stack(
+                  alignment: Alignment.bottomRight,
+                  children: <Widget>[
+                    Image.asset("assets/cardImg.png"),
+                    Padding(
+                      padding: const EdgeInsets.all(40),
+                      child: Text(
+                        "Flutter rules?",
+                        style: Theme.of(context).textTheme.title,
+                      ),
+                    ),
+                  ],
+                ),
+              ],
+            ),
+          );
+        },
+      ),
+      bottomSheet: SolidBottomSheet(
+        controller: _controller,
+        headerBar: Container(
+          color: Theme.of(context).primaryColor,
+          height: 50,
+          child: Center(
+            child: Text("Swipe me!"),
+          ),
+        ),
+        body: Container(
+          color: Colors.white,
+          height: 30,
+          child: Center(
+            child: Text(
+              "Hello! I'm a bottom sheet :D",
+              style: Theme.of(context).textTheme.display1,
+            ),
+          ),
+        ),
+      ),
+      floatingActionButton: FloatingActionButton(
+        child: Icon(Icons.stars),
+        onPressed: () {
+          _controller.isOpened ? _controller.hide() : _controller.show();
+        }
+      ),
     );
   }
 }
