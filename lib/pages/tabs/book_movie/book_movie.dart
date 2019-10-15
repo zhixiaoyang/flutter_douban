ba4092f0f6d9a96bba1d5ca3fd0506db55123e8d
Jahn <jahnli@163.com>
2019-10-15 12:08:05 +0800

    取消android状态栏阴影

--- lib/pages/tabs/book_movie/book_movie.dart
+++ lib/pages/tabs/book_movie/book_movie.dart
@@ -1,3 +1,5 @@
+import 'dart:io';
+
 import 'package:flutter/material.dart';
 import 'package:flutter/services.dart';
 import 'package:flutter_jahn_douban/pages/tabs/book_movie/movie/movie.dart';
@@ -20,6 +22,11 @@ class _BookMoviePageState extends State<BookMoviePage> with SingleTickerProvider
   void initState() { 
     super.initState();
     _tabController = TabController(length: _tabsList.length,vsync: this);
+     if (Platform.isAndroid) {
+     // 以下两行 设置android状态栏为透明的沉浸。写在组件渲染之后，是为了在渲染后进行set赋值，覆盖状态栏，写在渲染之前MaterialApp组件会覆盖掉这个值。
+      SystemUiOverlayStyle systemUiOverlayStyle = SystemUiOverlayStyle(statusBarColor: Colors.transparent);
+      SystemChrome.setSystemUIOverlayStyle(systemUiOverlayStyle);
+    }
   }
 
   @override
